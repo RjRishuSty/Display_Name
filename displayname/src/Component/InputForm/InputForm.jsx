61aa35e88fe85baf27d6828fc4db5e09ac53c96f
Your Name <rishurajk59@gmail.com>
2024-10-02 17:03:50 +0530

    add Project

---
+++ displayname/src/Component/InputForm/InputForm.jsx
@@ -0,0 +1,51 @@
+import React, { useRef, useState } from "react";
+import Styles from "./InputForm.module.css";
+const InputForm = () => {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [fullname, setfullname] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (firstname && lastname) {
+      setfullname(`${firstname} ${lastname}`);
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className={Styles.card}>
+        <h2 className={Styles.heading}>Full Name Display</h2>
+        <form onSubmit={submitHandler}>
+          <div>
+            <label className={Styles.inputLabel}>First Name:</label>
+            <input
+              className={Styles.input}
+              type="text"
+              placeholder="Enter your fistname"
+              onChange={(e) => setFirstname(e.target.value)}
+              required
+            />
+            <br />
+            <label className={Styles.inputLabel}>Last Name:</label>
+            <input
+              className={Styles.input}
+              type="text"
+              placeholder="Enter your lastname"
+              onChange={(e) => setLastname(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+        {fullname ? (
+          <div className={Styles.display}>
+            <h1>Full Name : {`${fullname}`}</h1>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default InputForm;
---
+++ displayname/src/Page/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import InputForm from "../Component/InputForm/InputForm";
+const Home = () => {
+  return (
+    <div>
+      <InputForm />
+    </div>
+  );
+};
+
+export default Home;
